b9afbd953e053d0a009c834ac8cfb907b3847675
phritnakgate <boss5500216@gmail.com>
2025-04-24 13:45:42 +0700

    learned logcat

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstandroidapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+
+        //LOGCAT
+        Log.v("Test Tag Verbose", "MSG")
+        Log.d("Test Tag Debug", "MSG")
+        Log.i("Test Tag Info", "MSG")
+        Log.w("Test Tag Warning", "MSG")
+        Log.e("Test Tag Error", "MSG")
+        Log.wtf("Test Tag Assert", "MSG")
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
