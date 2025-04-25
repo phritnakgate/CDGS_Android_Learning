eb2365741cfb765281c238c6c663527152663585
phritnakgate <boss5500216@gmail.com>
2025-04-25 12:03:45 +0700

    learn intent and eventlistener

--- app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstandroidapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.myfirstandroidapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,21 +14,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.basic_components)
+        setContentView(R.layout.landing_page)
 
-        val txtV = findViewById<TextView>(R.id.bsc_txt)
-        txtV.text = "Welcome to Android Development"
 
+        val loginBtn = findViewById<Button>(R.id.lnd_login)
+        loginBtn.setOnClickListener {
+            Log.d("Login Button", "Login button clicked")
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        val signUpBtn = findViewById<Button>(R.id.lnd_register)
+        signUpBtn.setOnLongClickListener {
+            Log.d("Sign Up Button", "Sign up button long clicked")
+            true
+        }
 
         //LOGCAT
-        Log.v("Test Tag Verbose", "MSG")
-        Log.d("Test Tag Debug", "MSG")
-        Log.i("Test Tag Info", "MSG")
-        Log.w("Test Tag Warning", "MSG")
-        Log.e("Test Tag Error", "MSG")
-        Log.wtf("Test Tag Assert", "MSG")
+//        Log.v("Test Tag Verbose", "MSG")
+//        Log.d("Test Tag Debug", "MSG")
+//        Log.i("Test Tag Info", "MSG")
+//        Log.w("Test Tag Warning", "MSG")
+//        Log.e("Test Tag Error", "MSG")
+//        Log.wtf("Test Tag Assert", "MSG")
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.basic_comp)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.landing_page)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
